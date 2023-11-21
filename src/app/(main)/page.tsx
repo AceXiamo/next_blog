36deca48946ecd4712df41065b7dd4340df09fc8
AceXiamo <xiamo@qwq.link>
2023-11-21 17:43:54 +0800

    feat: home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,32 +1,26 @@
 "use client";
 
 import Image from "next/image";
-import ToastBtn from "@/app/(main)/ToastBtn";
-import DialogBtn from "@/app/(main)/DialogBtn";
-import { Icon } from "@iconify/react";
-import Dependencies from "@/app/(main)/Dependencies";
+import { useEffect } from "react";
+import Light from "@/components/Light";
+import HomeInfo from "@/components/HomeInfo";
 
 export default function Home() {
+  useEffect(() => {
+    Light.generate();
+  });
+
   return (
-    <div className="flex flex-col items-center">
-      <Image alt="avatar" className="rounded-md" width={100} height={100} src={`https://axm.moe/avatar`}></Image>
-
-      <div className="mt-[50px] flex flex-col items-center gap-[20px]">
-        <div className="text-white flex gap-[10px] items-center">
-          <Icon icon="devicon:npm-wordmark" className="text-[50px]" />
-        </div>
-        <Dependencies />
-      </div>
-
-      <div className="mt-[50px] flex flex-col items-center gap-[20px]">
-        <div className="text-white flex gap-[10px]">
-          <span className="text-[20px]">⚒️</span>
-          <span className="text-[20px]">Components</span>
-        </div>
-        <div className="flex gap-[30px]">
-          <ToastBtn />
-          <DialogBtn />
-        </div>
+    <div className="flex flex-col p-[100px]">
+      <Image
+        alt="avatar"
+        className="rounded-full"
+        width={60}
+        height={60}
+        src={`https://axm.moe/avatar`}
+      ></Image>
+      <div className="selection:bg-emerald-400/10 mt-[20px]">
+        <HomeInfo />
       </div>
     </div>
   );
