e49b0fbbebda1b69cbb1ed428a812fda4d207d33
AceXiamo <xiamo@qwq.link>
2023-11-22 17:15:17 +0800

    refactor: code format

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,34 @@
+"use client";
+
 import React from "react";
 import HeadBar from "@/app/(main)/HeadBar";
+import { useEffect } from "react";
+import Light from "@/components/Light";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  useEffect(() => {
+    Light.generate();
+  });
+
   return (
-    <div className="flex flex-col relative">
+    <div className="flex flex-col relative overflow-x-hidden">
       <HeadBar />
-      <div>{children}</div>
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: "easeInOut", duration: 0.3 }}
+      >
+        {children}
+      </motion.div>
     </div>
   );
 }
