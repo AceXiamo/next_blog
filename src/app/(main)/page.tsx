52437e61d4fccff16493702d32b30399826b95a1
AceXiamo <xiamo@qwq.link>
2023-11-22 17:15:33 +0800

    Remove unused code and import

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect } from "react";
-import Light from "@/components/Light";
 import HomeInfo from "@/components/HomeInfo";
 
 export default function Home() {
-  useEffect(() => {
-    Light.generate();
-  });
-
   return (
     <div className="flex flex-col p-[100px]">
       <Image
